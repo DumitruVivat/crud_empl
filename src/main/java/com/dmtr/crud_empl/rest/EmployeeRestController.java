43510577409f398f5e0412326d3dc3f781d2e78f
Dumitru Vivat <kaibaman94@gmail.com>
2024-09-27 20:12:52 +0300

    add put mapping

--- src/main/java/com/dmtr/crud_empl/rest/EmployeeRestController.java
+++ src/main/java/com/dmtr/crud_empl/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.dmtr.crud_empl.rest;
 import com.dmtr.crud_empl.entity.Employee;
 import com.dmtr.crud_empl.service.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,24 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return employeeService.findAll();
+    }
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee employee = employeeService.findEmployeeById(employeeId);
+        if (employeeService == null){
+            throw  new RuntimeException("Employee with id=" + employeeId + " not found.");
+        } else {
+            return employee;
+        }
+    }
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee){
+        employee.setId(0);
+        return employeeService.save(employee);
+    }
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        Employee updatedEmployee = employeeService.save(employee);
+        return updatedEmployee;
     }
 }
