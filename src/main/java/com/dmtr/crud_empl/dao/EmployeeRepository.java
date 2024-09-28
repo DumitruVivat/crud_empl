0f8e5758896d026ff04fcd06cf5a93257a2f4642
Dumitru Vivat <kaibaman94@gmail.com>
2024-09-28 15:40:41 +0300

    add repository

--- src/main/java/com/dmtr/crud_empl/dao/EmployeeRepository.java
+++ src/main/java/com/dmtr/crud_empl/dao/EmployeeRepository.java
@@ -2,9 +2,9 @@ package com.dmtr.crud_empl.dao;
 
 import com.dmtr.crud_empl.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
+@RepositoryRestResource(path = "empls")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
